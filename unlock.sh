8e05aeaf80fb0d8af543f5aa6cbe65a371ccca7a
David Noren <dcnoren@gmail.com>
2016-12-03 12:47:01 -0500

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 $USERNAME = user
 $PASSWORD = pass
-$DOOR_ID = 110
-$URL = 192.168.10.10
+$DOOR_ID = DOOR_ID_HERE
+$URL = IP_ADDRESS_HERE
 SESSIONID=$(curl -X POST --data "debug=0&fldUserName=$USERNAME&fldPassword=$PASSWORD" --header "Content-Length: 55" http://$URL/Site_SMS/SF_User_Login/cgi-bin/process_login.cgi -v 2>&1 | grep Location | cut -c51-58)
 echo "${SESSIONID}"
 curl -X POST --data "hfldSessionId=$SESSIONID&debug=0&statusResponse=1&hfldButton=executeMRO&hfldDeviceId=$DOOR_ID&hfldDeviceList=&hfldMROTypeId=1&hdlfDuration=10" http://$URL/Site_SMS/SF_SSC/cgi-bin/sms-ssh.cgi
